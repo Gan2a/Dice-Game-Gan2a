972907b5e36e7b576c05d892834733faab6c9679
gan2a <sharavpil.gantulga@gmail.com>
2021-01-05 14:37:56 +0800

    shoo shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var score = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
+
 //<div class="player-score" id="score-0">43</div>
 //window.document.querySelector("#score-0").textContent = dice;
 // Тоглоом эхлэхэд бэлтгэх
@@ -14,7 +14,11 @@ document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
-document.querySelector(".dice").style.display = "none";
-document.querySelector("btn-roll").addEventListener("click", shooShid);
-shooShid;
-console.log("Шоо: " + dice);
+
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
